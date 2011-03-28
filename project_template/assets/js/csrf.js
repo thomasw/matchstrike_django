15bf1eadb980accf46c8f40f678e58fc8a1c505b
thomasw <thomas@matchstrike.net>
2011-03-28 01:13:03 -0400

    Added the recommended JS for attaching csrf tokens to ajax requests.

---
+++ project_template/assets/js/csrf.js
@@ -0,0 +1,26 @@
+/* Add the CSRF token as an X-CSRFToken header to AJAX POST requests
+Source: http://docs.djangoproject.com/en/1.2/ref/contrib/csrf/#ajax
+*/
+$.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+                function getCookie(name) {
+                        var cookieValue = null;
+                        if (document.cookie && document.cookie != '') {
+                                var cookies = document.cookie.split(';');
+                                for (var i = 0; i < cookies.length; i++) {
+                                        var cookie = jQuery.trim(cookies[i]);
+                                        // Does this cookie string begin with the name we want?
+                                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                                                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                                break;
+                                        }
+                                }
+                        }
+                        return cookieValue;
+                }
+                if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                        // Only send the token to relative URLs i.e. locally.
+                        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                }
+        }
+});
